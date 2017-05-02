5e2a1fa9db7ce622f839e6fa47602eb371beb2ad
Sergio MT <sergio.mt88@gmail.com>
2017-05-02 14:38:44 +0200

    homeController.js + index

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,6 +1,13 @@
 angular.module('gameApp')
 	
-  .controller('homeController', function ($scope, gameAppService ){
-     $scope.gameSearch = '' // That recives the input value from ng-model(index.html)
-
+  .controller('homeController', function ($scope, gameAppService){ 	// Function with '$scope'(from html) and 'gameAppService' from service.js
+     $scope.gameInput = '' 	// That recives the input value from ng-model(index.html)
+     $scope.searchGame = function() {
+     	console.log("button")
+     	gameAppService.searchGame($gameInput)
+     	.then(function (response) {
+     		$scope.gamelist = response.ALGO
+     		console.log($scope.gamelist)
+     	})
+     }
 	})
